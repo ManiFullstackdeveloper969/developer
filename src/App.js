5c174e177c35e5f779b4a4a4447c5ef39ea5b623
ManiFullstackdeveloper969 <mmaniram017@gmail.com>
2024-06-09 01:16:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import TaskList from './components/TaskList';
+import "./components/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
